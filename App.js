d60b40ea201c130caadf9b13b6467813050d6fba
mendesjar <ga0301@outlook.com>
2022-10-04 22:12:29 -0300

    feat: develop branch

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Text>HackerMaster</Text>
+      <Text>develop branch</Text>
     </View>
   );
 }
